ae0a83ccf6425cc60ac371e38163e1c2948218dc
Saad Muhammad Abdul Ghani <saadhku2@gmail.com>
2019-10-07 14:17:44 +0800

    going to work on data augmentation. fixed main.py to correctly output stats files

--- preprocessing.py
+++ preprocessing.py
@@ -107,6 +107,37 @@ class RandomCrop(object):
 
         return {'image':image, 'label':sample['label']}
 
+# data augmentation techniques
+class Mirror(object):
+    """Crop randomly the image in a sample.
+
+    Args:
+        output_size (tuple or int): Desired output size. If int, square crop
+            is made.
+    """
+
+    def __init__(self, output_size):
+        assert isinstance(output_size, (int, tuple))
+        if isinstance(output_size, int):
+            self.output_size = (output_size, output_size)
+        else:
+            assert len(output_size) == 2
+            self.output_size = output_size
+
+    def __call__(self, sample):
+        image = sample['image']
+
+        h, w = image.shape[:2]
+        new_h, new_w = self.output_size
+
+        top = np.random.randint(0, h - new_h) 
+        left = np.random.randint(0, w - new_w)
+
+        image = image[top: top + new_h,
+                      left: left + new_w]
+
+        return {'image':image, 'label':sample['label']}
+
 
 # to convert numpy images to torch images
 class ToTensor(object):
